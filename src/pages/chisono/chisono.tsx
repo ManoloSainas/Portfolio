f167be1d6b11259d0cb1f8a9977bef60eaae40a5
Manolo Sainas <sainas.manolo@gmail.com>
2025-09-05 15:52:39 +0200

    Creata una map anche per le icone della pagina chi sono

--- src/pages/chisono/chisono.tsx
+++ src/pages/chisono/chisono.tsx
@@ -3,23 +3,39 @@ import { Stack } from '../../components/Stack'
 import { StyledChiSono } from './styled'
 
 export const ChiSono = () => {
+  const techImages = [
+    { alt: 'logo di react', imageUrl: 'src/assets/react-icon.png', width: 100 },
+    { alt: 'logo di typescript', imageUrl: 'src/assets/ts-icon.png', width: 40 },
+    { alt: 'logo di javascript', imageUrl: 'src/assets/JS-iconpng.png', width: 40 },
+    { alt: 'logo di html', imageUrl: 'src/assets/html-icon.png', width: 40 },
+    { alt: 'logo di css', imageUrl: 'src/assets/css-icon.png', width: 47 },
+    {
+      alt: 'logo di visual studio code',
+      imageUrl: 'src/assets/vscode-icon.png',
+      width: 35
+    },
+    { alt: 'logo di git', imageUrl: 'src/assets/git-icon.png', width: 40 }
+  ]
+
   return (
     <StyledChiSono>
       <Stack>
         <Image
           className="foto-manolo"
           alt="Foto di Manolo Sainas"
-          imageUrl="src\assets\myself.jpg"
+          imageUrl="src/assets/myself.jpg"
           width={170}
           height={175}
         />
       </Stack>
+
       <Stack flexDirection="column" gap="1rem">
         <h3>
           Mi sono recentemente laureato in Informatica all'Università di Cagliari.
           <br /> Durante il tirocinio universitario ho sviluppato
           <br /> un'applicazione front-end utilizzando React e TypeScript.
         </h3>
+
         <Stack
           gap="2rem"
           alignItems="center"
@@ -27,21 +43,14 @@ export const ChiSono = () => {
           backgroundColor="white"
           padding="1rem"
         >
-          <Image alt="logo di react" imageUrl="src\assets\react-icon.png" width={100} />
-          <Image alt="logo di typescript" imageUrl="src\assets\ts-icon.png" width={40} />
-          <Image
-            alt="logo di javascript"
-            imageUrl="src\assets\JS-iconpng.png"
-            width={40}
-          />
-          <Image alt="logo di html" imageUrl="src\assets\html-icon.png" width={40} />
-          <Image alt="logo di css" imageUrl="src\assets\css-icon.png" width={47} />
-          <Image
-            alt="logo di visual studio code"
-            imageUrl="src\assets\vscode-icon.png"
-            width={35}
-          />
-          <Image alt="logo di git" imageUrl="src\assets\git-icon.png" width={40} />
+          {techImages.map((img) => (
+            <Image
+              key={img.imageUrl}
+              alt={img.alt}
+              imageUrl={img.imageUrl}
+              width={img.width}
+            />
+          ))}
         </Stack>
       </Stack>
     </StyledChiSono>
